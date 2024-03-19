381d452e44e2d9863c15b68f2664425fdd00c3dd
Klumenko1401 <klymenko.yevheniia1121@vu.cdu.edu.ua>
2024-03-19 13:44:13 +0200

    The floor is lava! UI

---
+++ Assets/Scripts/Generics.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeClass
+{
+    public T GenericMethod<T>(T param)
+    {
+        return param;
+    }
+}
+public class SomeOtherClass : MonoBehaviour
+{
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+
+        myClass.GenericMethod<char>('k');
+    }
+}
+public class GenericClass<T>
+{
+    T item;
+
+    public void UpdateItem(T newItem)
+    {
+        item = newItem;
+    }
+}
+public class GenericClassExample : MonoBehaviour
+{
+    void Start()
+    {
+        GenericClass<char> myClass = new GenericClass<char>();
+
+        myClass.UpdateItem('k');
+    }
+}
---
+++ Assets/Scripts/polymorphism.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+// Базовий клас Сцена
+public class Scene
+{
+    public string Name { get; set; }
+
+    // Конструктор класу
+    public Scene(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void DisplayInfo()
+    {
+
+    }
+}
+
+// Клас Плити, похідний від Сцена
+public class Plates : Scene
+{
+    public int NumOfPlates { get; set; }
+
+    // Конструктор класу
+    public Plates(string name, int numOfPlates) : base(name)
+    {
+        NumOfPlates = numOfPlates;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+    }
+}
+
+// Клас Огорожа, похідний від Плити
+public class Fence : Plates
+{
+    public bool HasGate { get; set; }
+
+    // Конструктор класу
+    public Fence(string name, int numOfPlates, bool hasGate) : base(name, numOfPlates)
+    {
+        HasGate = hasGate;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+    }
+}
+
+// Клас Гра
+public class Game
+{
+    public static void Main()
+    {
+        // Створюємо об'єкти різних класів
+        Scene scene = new Scene("City Square");
+        Plates plates = new Plates("Park", 20);
+        Fence fence = new Fence("Backyard", 10, true);
+
+        // Викликаємо метод DisplayInfo() для кожного об'єкта, використовуючи поліморфізм
+        scene.DisplayInfo();
+
+        plates.DisplayInfo();
+
+        fence.DisplayInfo();
+    }
+}
